af42af75a782db5c9ce48db60d6bbb24caae230e
sangramrath <sangram.rath@gmail.com>
2019-04-02 12:20:13 +0530

    Update local-production.js

--- Azure/sharp/local-production.js
+++ Azure/sharp/local-production.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: {
-    uri: 'mongodb://<cosmosdb_name>:<primary_master_key>@<cosmosdb_name>.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false'
+    uri: 'mongodb://<cosmosdb_name>:<primary_master_key>@<cosmosdb_name>.documents.azure.com:10255/mean?ssl=true&sslverifycertificate=false'
   }
 };
